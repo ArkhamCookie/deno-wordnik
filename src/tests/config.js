abf20801c8cbd4344c1bd4275d62c7dd089ba4a2
ArkhamCookie <git@mail.theuntitledproject.dev>
2023-08-11 22:28:24 -0500

    splitting tests into different files

---
+++ src/tests/config.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env -S deno run --allow-read --allow-env
+
+import { assertExists } from 'assert'
+import 'dotenv'
+
+/** Vaildate Config File */
+Deno.test(
+	'Test: API key value', () => {
+		assertExists(Deno.env.get('WORKNIK_KEY'), 'API key value is not set')
+	}
+)
--- src/tests/vaildate.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env -S deno run
-// import
-
-/** Vaildate API Key */
-console.log('foo')
