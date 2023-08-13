c81d93842e4c487082d5fac7374ec961c45e1a08
ArkhamCookie <git@mail.theuntitledproject.dev>
2023-08-13 05:03:01 -0500

    fixed range errors + organizing response statuses

--- src/tests/vaildate.js
+++ src/tests/vaildate.js
@@ -1,13 +1,12 @@
 #!/usr/bin/env -S deno run --allow-read --allow-env --allow-net
-// deno-lint-ignore-file no-fallthrough
-/* eslint-disable no-fallthrough */
 
 /** Resources
- * [MDN - HTTP response status codes](https://developer.mozilla.org/en-US/docs/Web/HTTP/Status)
- * [iana - HTTP Status Code Registry](https://www.iana.org/assignments/http-status-codes/http-status-codes.xhtml)
- * [RTF - Additional HTTP Status Codes](https://www.rfc-editor.org/rfc/rfc6585.html)
- * [RTF - Extending HTTP](https://www.rfc-editor.org/rfc/rfc9110.html#name-extending-http)
- * [Wikipedia - Hyper Text Coffee Pot Control Protocol](https://en.wikipedia.org/wiki/Hyper_Text_Coffee_Pot_Control_Protocol)
+ * [HTTP response status codes - MDN](https://developer.mozilla.org/en-US/docs/Web/HTTP/Status)
+ * [HTTP Status Code Registry - iana](https://www.iana.org/assignments/http-status-codes/http-status-codes.xhtml)
+ * [Additional HTTP Status Codes - RTF](https://www.rfc-editor.org/rfc/rfc6585.html)
+ * [Extending HTTP - RTF](https://www.rfc-editor.org/rfc/rfc9110.html#name-extending-http)
+ * [IM Used - Datatracker](https://datatracker.ietf.org/doc/html/rfc3229#section-10.4.1)
+ * [I'm a teapot/Hyper Text Coffee Pot Control Protocol - Wikipedia](https://en.wikipedia.org/wiki/Hyper_Text_Coffee_Pot_Control_Protocol)
 */
 
 import { AssertionError } from 'assert'
@@ -16,80 +15,143 @@ import 'dotenv'
 const base = 'https://api.wordnik.com/v4/'
 const key = '?api_key=' + Deno.env.get('WORDNIK_KEY')
 
-/** Vaildate Connection Status
+/** HTTP Status
+ * Vaildate Connection Status
  * @param {string} [target] - change fetch target
  * @param {Object} [allowType] - choose which special reponses to allow
- * @param {boolean} allowType.webdav - allow WebDAV responses
- * @param {boolean} allowType.im - allow IM Used response
- * @param {boolean} allowType.deprecated - allow deprecated responses
+ * @param {boolean} [allowType.webdav] - allow WebDAV responses
+ * @param {boolean} [allowType.im] - allow IM Used response
+ * @param {boolean} [allowType.deprecated] - allow deprecated responses
  * @param {boolean|intiger} [unreconized] - allow unreconized (or custom) responses
+ * @param {string}
 */
 
 Deno.test({
 	name: 'Test: HTTP Status',
-	async fn(target, allowType, unreconized) {
-		// if (target === null) { const target = 'words.json/wordOfTheDay' }
-		// target = 'words.json/wordOfTheDay'
-		target = 'foo'
+	async fn(target, failOn, allowType, unreconized) {
+		// Get config
+		if (target === undefined) { target = 'words.json/wordOfTheDay' }
+		if (failOn === undefined) {
+			failOn = {
+				content: false
+			}
+		}
+		if (allowType === undefined) {
+			allowType = {
+				webdav: false,
+				im: false,
+				deprecated: false
+			}
+		} else {
+			let json = JSON.stringify(allowType)
+			json = JSON.parse(json)
+			console.debug('allowType:', json)
+		}
+
+		// Get Response
 		const response = (await fetch(
 			base + target + key
 		))
-		const status = response.status
-		console.debug(status)
+		// const status = response.status
+		const status = 305
+		if (response) { await response.body.cancel() }
 
+		/**
+		 * Check Response Status
+		 * @returns {boolean}
+		*/
 		switch (true) {
 		case (isNaN(status)):
-			throw new AssertionError('Status: ' + response.status + 'is not a number!')
+			throw new AssertionError('Status is not a number!')
+
+		/** Success Statuses */
+		// Basic
 		case (status === 200): // Successful
-			console.debug('Response: ' + status)
-			break
-		case (204 <= status <= 225): // Part successful
-			// console.debug('Response: ' + response.statusText)
-			console.log('Part: ' + response.statusText)
+			console.debug('Successfully Connected')
+			return true
+
+		case (status === 203): // Non-Authoritative Information
+		case (status === 205): // Reset Content (UI, form, canvas, etc)
+			console.debug(status + ':', response.statusText)
+			return true
+
+		// Part/No Content
+		case (status === 204): // No Content
+		case (status === 206): // Partial Content
+			console.debug(status + ':', response.statusText)
 			break
 
-		// Standard Error HTTP Responses
-		case (300 <= status <= 304 || status === 308): // Redirection messages
-			console.debug('Redirecting (' + status + ')... ')
-			// throw new AssertionError('Redirecting: ' + response.status)
-		case (400 <= status <= 417 || 421 <= status <= 426 || 428 <= status <= 429 || status === 451): // Client error responses
-			throw new AssertionError('Client error: ' + response.status)
-		case (500 <= status <= 508 || status === 511): // Server error responses
-			throw new AssertionError('Deprecated:' + response.status)
+		/** Redirection Messages */
+		case (status === 300): // Multiple Choices
+			console.debug(status + ':', response.statusText)
+			break
 
-		case (status === 207 || status === 208): // WebDAV Responses
-			console.debug('WebDAV Response, Code: ' + status)
+		case (status === 304): // Not Modified (for caching)
+			console.debug(status + ':', response.statusText)
+			break
+
+		case (status === 301): // Moved Permanently
+		case (status === 302): // Moved Temporarily
+		case (status === 303): // See Other
+		case (status === 307): // Temporary Redirect
+		case (status === 308): // Permanent Redirect
+			// console.debug('Redirecting (' + status + ')... ')
+			console.debug(status + ':', response.statusText)
+			break
+
+		/** Client error statuses */
+		case (400 <= status && status <= 417):
+		case (421 <= status && status !== 418 && status <= 426):
+		case (status === 428):
+		case (status === 429):
+		case (status === 451):
+			console.debug(status + ':', response.statusText)
+			throw new AssertionError('Client error')
+		case ((500 <= status && status <= 508) || status === 511): // Server error statuses
+			console.debug(status + ':', response.statusText)
+			throw new AssertionError('Server error')
+
+		/** Non-standard Statuses */
+		// WebDAV Statuses
+		case (status === 207 || status === 208):
+			console.debug('WebDAV Response. Status:', status)
 			if (allowType.webdav === true) {
-				console.debug()
 				break
 			}
-		case (status === 226): // IM Used
+			throw new AssertionError('WebDAV')
+		// IM Used Status
+		case (status === 226):
+			console.debug(status + ':', response.statusText)
 			if (allowType.im === true) {
-				console.debug()
 				break
 			}
+			throw new AssertionError('IM Used')
+		// I'm a teapot Status
 		case (status === 418):
-			if (allowType.teapot) {
+			console.debug(status + ':', response.statusText)
+			if (allowType.teapot === true) {
 				break
 			}
-			throw new AssertionError('Deprecated' + response.statusText)
-		case (status === 305 || status === 306 || status === 418 || status === 510):
+			throw new AssertionError("I'm a teapot")
+
+		/** Deprecated Statuses */
+		case (status === 305 || status === 306 || status === 510):
+			// console.error('Deprecated. Status: ', status)
+			console.error('Deprecated.')
+			console.error(status + ':', response.statusText)
 			if (allowType.deprecated === true) {
-				console.error('Deprecated status code: ' + status)
 				break
 			}
-			throw new AssertionError('Deprecated: ' + response.status)
-		default: // Unreconized
-			console.debug('Custom Response status?')
-			if (unreconized === true) { // will allow you to specify in the future
-				console.debug('Response Code: ' + status)
-				break
-			}
-			console.error('Unreconized Response Status: ' + status)
+			throw new AssertionError('Deprecated')
+
+		/** Unreconized Statuses */
+		case (unreconized === true):
+		case (unreconized):
+			break
+		default:
+			// console.debug('Custom Response status?')
+			console.debug(status + ':', 'Unknown Response Status')
 			throw new AssertionError('Connection failed!\n' + response.statusText)
 		}
-		const _close = response.text()
-		// console.log('Status: ' + status)
-		// console.log(response.json)
 	}
 })
