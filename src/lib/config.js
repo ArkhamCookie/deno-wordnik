504569183543720cb7e47074e3de0ef019907377
ArkhamCookie <git@mail.theuntitledproject.dev>
2023-08-18 17:57:45 -0500

    fixed export of 'getKey()' and typo

--- src/lib/config.js
+++ src/lib/config.js
@@ -3,21 +3,18 @@ import 'dotenv'
 /**
  * Module to setup your config
  * @module @worknik/conifg
- * @exports key - auth suffex + API key
+ * @exports getKey - auth suffex + API key
  */
 
-module.exports = {
-	/**
-	 * Get API Key
-	 * @returns {string} key
-	*/
-
-	key: function() {
-		if (Deno.env.has('WORKNIK_KEY')) {
-			const key = '?api_key=' + Deno.env.get('WORDNIK_KEY')
-			return key
-		} else {
-			console.error('No API key found!')
-		}
+/**
+ * Get API Key
+ * @returns {string} key
+*/
+export function getKey() {
+	if (Deno.env.has('WORDNIK_KEY')) {
+		const key = '?api_key=' + Deno.env.get('WORDNIK_KEY')
+		return key
+	} else {
+		console.error('No API key found!')
 	}
 }
