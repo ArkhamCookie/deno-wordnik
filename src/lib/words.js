1b5847bcf4a9db2bc42e9e52722ef8bb36efdfbe
ArkhamCookie <git@mail.theuntitledproject.dev>
2023-08-18 19:40:31 -0500

    added word lib file + started on wordOfTheDay()

---
+++ src/lib/words.js
@@ -0,0 +1,37 @@
+import { buildTarget } from '../mod.js'
+
+/**
+ * Random Word - Return a single random word/WordObject
+ * @param {boolean} [hasDictionaryDef] - Only return words dictionary definitions
+ * @param {string} [includePartOfSpeech] - CSV part-of-speech values in include
+ * @param {string} [excludePartOfSpeech] - CSV part-of-speech values in exclude
+ * @param {integer} [minCorpusCount] - Minimum corpus frequency for terms
+ * @param {integer} [maxCorpusCount] - Maximum corpus frequency for terms
+ * @param {integer} [minDictionaryCount] - Minimum dictionary count
+ * @param {integer} [maxDictionaryCount] - Maximum dictionary count
+ * @param {integer} [minLength] - Minimum word length
+ * @param {integer} [maxLength] - Maximum word length
+ */
+export function random() {
+	console.debug('random')
+}
+
+/**
+ * Word of the Day
+ * @param {string} [date] - Fetches by date (in yyyy-MM-dd format)
+ */
+export async function wordOfTheDay(date) {
+	let response
+
+	if (date) {
+		response = await fetch(buildTarget('/words.json/wordOfTheDay' + '?date=' + date + '&'))
+	} else if (!date) {
+		response = await fetch(buildTarget('/words.json/wordOfTheDay?'))
+	}
+
+	if (response.status !== 200) {
+		console.error('Error! Status:', response.statusText)
+		return
+	}
+	console.log(await response.json())
+}
--- src/mod.js
+++ src/mod.js
@@ -3,3 +3,4 @@
  */
 
 export * from './lib/config.js'
+export * from './lib/words.js'
