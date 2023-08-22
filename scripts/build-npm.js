0e25686f1ac0e81ee01341ee7525e64244ecd598
ArkhamCookie <git@mail.theuntitledproject.dev>
2023-08-21 21:03:26 -0500

    Created basic/example build script file
    https://github.com/denoland/dnt#setup

---
+++ scripts/build-npm.js
@@ -0,0 +1,29 @@
+import { build, emptyDir } from 'https://deno.land/x/dnt@0.38.1/mod.ts'
+
+await emptyDir('./npm')
+
+await build({
+	entryPoints: ['./src/mod.js'],
+	outDir: './npm',
+	shims: {
+		deno: true
+	},
+	package: {
+		name: 'deno-wordnik',
+		version: Deno.args[0],
+		description: '',
+		license: 'AGPL-3.0-or-later',
+		repository: {
+			type: 'git',
+			url: 'git+https://github.com/ArkhamCookie/deno-wordnik.git'
+		},
+		bugs: {
+			url: 'https://github.com/ArkhamCookie/deno-wordnik/issues'
+			// 'https://github.com/ArkhamCookie/deno-wordnik/issues/new?assignees=&labels=bug&projects=&template=BUG_REPORT.yaml&title=%5BBUG%5D%3A+'
+		}
+	},
+	postBuild() {
+		Deno.copyFileSync('LICENSE', 'npm/LICENSE')
+		Deno.copyFileSync('src/README.md', 'npm/README.md')
+	}
+})
